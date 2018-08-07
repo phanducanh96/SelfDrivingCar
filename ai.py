1b804e06fa6e79f23bcbe9e9fd1707eab40ed883
Duc Anh Phan <phanducanh96@gmail.com>
2018-08-07 11:56:37 -0500

    Recreating the ai file. Had Network class done and initialized replayMemory class. Added lots more comment in order for understanding in the future

--- ai.py
+++ ai.py
@@ -1,107 +1,73 @@
-# AI for Self Driving Car
-
-# Importing the libraries
+#AI
+#Importing libraries
 
 import numpy as np
 import random
+#load and save the model
 import os
+#best library for neural networks since it can handle dynamic graph
 import torch
+#module contains all tools for the neural networks
 import torch.nn as nn
+#import all types of functions in the neural networks
 import torch.nn.functional as F
+#Optimizer library to performing gradient descent
 import torch.optim as optim
+#...
 import torch.autograd as autograd
 from torch.autograd import Variable
 
-# Creating the architecture of the Neural Network
+#Creating the architecture of the neural network
 
+#Child class of a class nn.Module (using inheritance in this case)
 class Network(nn.Module):
     
-    def __init__(self, input_size, nb_action):
+    def __init__(self, inputSize, nbAction):
+        #this function inherits from the nn module, uses all the tool from the nn module
         super(Network, self).__init__()
-        self.input_size = input_size
-        self.nb_action = nb_action
-        self.fc1 = nn.Linear(input_size, 30)
-        self.fc2 = nn.Linear(30, nb_action)
-    
+        self.inputSize = inputSize
+        self.nbAction = nbAction
+        
+        #Now we need to specify the connections between the layers
+        #We have 2 full connections. Input Layer to Hidden Layer and Hidden Layer to Output Layer
+        
+        #This function fully connects the neurons from input layer to neurons in hidden layer
+        #Note that the second param is mainly based on testing, but now we are putting 30 since it gave
+        #the best results; however, feel free to change or whatsoever
+        self.fc1 = nn.Linear(inputSize, 30)
+        #First param here is 30 since its between hidden layer to ouput layer.
+        #Hidden layer again has 30 hidden neurons because of many testing
+        #Output neurons are based on how many actions the object has
+        self.fc2 = nn.Linear(30, nbAction)
+     
+    #function to activate the neurons, return the Q value
     def forward(self, state):
+        #activate the hidden neuron
+        #state in this case is represented as a hidden state
+        #Input neuron
         x = F.relu(self.fc1(state))
+        #Q-Value for the output neuron
         q_values = self.fc2(x)
         return q_values
-
-# Implementing Experience Replay
-
+    
+#Implementing Experience Replay
 class ReplayMemory(object):
     
     def __init__(self, capacity):
-        self.capacity = capacity
+        #capacity is the maximum number of transitions we want to have in our memory of events
+        self.capacity  = capacity
         self.memory = []
-    
-    def push(self, event):
-        self.memory.append(event)
-        if len(self.memory) > self.capacity:
-            del self.memory[0]
-    
-    def sample(self, batch_size):
-        samples = zip(*random.sample(self.memory, batch_size))
-        return map(lambda x: Variable(torch.cat(x, 0)), samples)
-
-# Implementing Deep Q Learning
-
-class Dqn():
-    
-    def __init__(self, input_size, nb_action, gamma):
-        self.gamma = gamma
-        self.reward_window = []
-        self.model = Network(input_size, nb_action)
-        self.memory = ReplayMemory(100000)
-        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
-        self.last_state = torch.Tensor(input_size).unsqueeze(0)
-        self.last_action = 0
-        self.last_reward = 0
-    
-    def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
-        action = probs.multinomial()
-        return action.data[0,0]
-    
-    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
-        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        next_outputs = self.model(batch_next_state).detach().max(1)[0]
-        target = self.gamma*next_outputs + batch_reward
-        td_loss = F.smooth_l1_loss(outputs, target)
-        self.optimizer.zero_grad()
-        td_loss.backward(retain_variables = True)
-        self.optimizer.step()
-    
-    def update(self, reward, new_signal):
-        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
-        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
-        action = self.select_action(new_state)
-        if len(self.memory.memory) > 100:
-            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
-            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
-        self.last_action = action
-        self.last_state = new_state
-        self.last_reward = reward
-        self.reward_window.append(reward)
-        if len(self.reward_window) > 1000:
-            del self.reward_window[0]
-        return action
-    
-    def score(self):
-        return sum(self.reward_window)/(len(self.reward_window)+1.)
-    
-    def save(self):
-        torch.save({'state_dict': self.model.state_dict(),
-                    'optimizer' : self.optimizer.state_dict(),
-                   }, 'last_brain.pth')
-    
-    def load(self):
-        if os.path.isfile('last_brain.pth'):
-            print("=> loading checkpoint... ")
-            checkpoint = torch.load('last_brain.pth')
-            self.model.load_state_dict(checkpoint['state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer'])
-            print("done !")
-        else:
-            print("no checkpoint found...")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
